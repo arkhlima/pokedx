a30947f59685f4202ed6272742187e51a16dca19
arkhlima <arkhlima@gmail.com>
2022-06-22 19:07:12 +0700

    add: button component & filter button on index navigation

---
+++ components/Btn.vue
@@ -0,0 +1,12 @@
+<template>
+  <button
+    class="button [ flex items-center rounded-md focus:ring focus:ring-gray-300 focus:outline-none py-0.5 px-2 ]"
+    @click="$emit('click')"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup>
+  defineEmits(['click']);
+</script>
