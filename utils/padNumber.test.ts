a3a4630729d98953c61fdb1ee61661ac3b2e8d52
arkhlima <arkhlima@gmail.com>
2022-06-22 21:03:22 +0700

    add: util for pokemon id (number) padding & unit test

---
+++ utils/padNumber.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, it } from 'vitest'
+import padNumber from './padNumber'
+
+describe('padNumber', () => {
+  it.each([
+    [5, '#005'],
+    [55, '#055'],
+    [555, '#555'],
+    [5555, '#5555'],
+  ])('Should return value correctly', (input, output) => {
+    expect(padNumber(input)).toEqual(output)
+  })
+})
---
+++ utils/padNumber.ts
@@ -0,0 +1,5 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+// e.g. 5 -> #005
+export default function (val: number) {
+  return `#${String(val).padStart(3, '0')}`;
+};
