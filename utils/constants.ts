15a53f25a8def24cff5a15eba04a9dc8b0db12f2
arkhlima <arkhlima@gmail.com>
2022-06-24 04:11:44 +0700

    revert

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,4 +1,4 @@
-export const POKEMON_IMG_BASE_URL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/`
+export const POKEMON_IMG_BASE_URL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/`
 
 export const POKEMON_TYPE_COLOR: Record<string, string> = {
 	normal: 'bg-stone-500',
